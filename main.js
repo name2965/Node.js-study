b71a4717d3e713042a122bfb836b7e9941b63d8f
name2965 <66663783+name2965@users.noreply.github.com>
2023-01-31 23:35:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,55 +7,35 @@ var app = http.createServer(function(request,response){
     var title = queryData.id;
     if(_url == '/'){
         title = 'Welcome';
-        fs.readFile(`data/Welcome`,'utf-8',function(err,description){
-            var template = `
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ul>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">Javascript</a></li>
-                </ul>
-                <h2>${title}</h2>
-                <p>${description}</p>
-            </body>
-            </html> 
-            `;
-            response.end(template);
-        })
-    }else{
+    }
         if(_url == '/favicon.ico'){
             return response.writeHead(404);
         }
-        response.writeHead(200);
-        fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
-            var template = `
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ul>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">Javascript</a></li>
-                </ul>
-                <h2>${title}</h2>
-                <p>${description}</p>
-            </body>
-            </html> 
-            `;
-            response.end(template);
-        })
-    }
+    response.writeHead(200);
+    fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+        if(!description){
+            description = fs.readFileSync(`data/Welcome`,'utf-8');
+        }
+        var template = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">Javascript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+        </body>
+        </html> 
+        `;
+        response.end(template);
+    })
 });
 app.listen(3000);
